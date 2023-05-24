018f92fb2d48fd6557add1817dace6700c87fa64
Grishmita <grishmitapandey@gmail.com>
2023-05-24 06:30:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+
+
+
 
 function App() {
+    let [counter , setCounter] = useState(0)
+
+    const increment = () =>{
+      setCounter(
+        counter = counter + 1
+      )
+    }
+
+    const decrement = () =>{
+      setCounter(
+        counter = counter - 1
+      )
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={increment}>
+      Increment
+    </button>  
+      {counter}
+    <button onClick={decrement}>
+      Decrement
+    </button>  
     </div>
   );
 }
